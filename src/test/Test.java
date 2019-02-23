7a2d17b27544a11220db7addd9d3caebcf2c685f
Egor Dorichev <egordorichev@gmail.com>
2019-02-23 09:07:41 +0300

    Casting issues

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,11 +1,9 @@
 package test;
 
-import java.util.ArrayList;
-
-public class Test<T extends A> {
-	public <C> C test() {
-		ArrayList<T, C> list = new ArrayList<>();
-		test<T, C>();
-		// todo: type casts
+public class Test {
+	public void test() {
+		int a = (int) 32.1; // 32
+		int b = (int) -32.1; // -32
+		double c = (a) -32.1; // a - 32.1
 	}
 }
